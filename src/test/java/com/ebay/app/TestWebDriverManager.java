6bb5a755c8ed1c5ba14bc09891fc833d339c5242
Pedro Jara <pedrojara2405@gmail.com>
2018-10-26 02:00:47 -0500

    Adding dependencies and TestWebDriverManager class

---
+++ src/test/java/com/ebay/app/TestWebDriverManager.java
@@ -0,0 +1,31 @@
+package com.ebay.app;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+public class TestWebDriverManager {
+
+    private WebDriver _webDriver;
+
+    @BeforeClass
+    public void beforeClass() {
+
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+
+    }
+
+    @BeforeMethod
+    public void afterClass() {
+
+    }
+}
